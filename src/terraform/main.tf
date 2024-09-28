7d29cd30f16103a65ed6faefd24edfc231e57766
Anderson Souza <andersonssilveira96@gmail.com>
2024-09-28 17:42:21 -0300

    Updating API Gateway lambda reference

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -6,10 +6,10 @@ module "lambda" {
 module "apiGateway" {
   source               = "./modules/apiGateway"
   arn_lambda_auth      = module.lambda.lambda_arn_auth
-  arn_lambda_cadastro  = module.lambda.lambda_arn_signup
+  arn_lambda_signup    = module.lambda.lambda_arn_signup
   environment          = var.environment
   nome_lambda_auth     = module.lambda.nome_lambda_auth
-  nome_lambda_cadastro = module.lambda.nome_lambda_signup
+  nome_lambda_signup   = module.lambda.nome_lambda_signup
 }
 
 
